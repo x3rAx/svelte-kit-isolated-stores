4711858e19b9eb6bfeb4a9128ab7f94dee0edd6b
^x3ro <git@x3ro.dev>
2022-01-05 12:13:17 +0100

    📦 NEW: Shorthands to define simple readable and writable stores

--- src/defineStore.ts
+++ src/defineStore.ts
@@ -1,4 +1,5 @@
 import { useSession } from './useSession'
+import { writable, readable, StartStopNotifier, Writable, Readable } from 'svelte/store'
 
 export function defineStore<T>(fn: () => T): () => T {
     return () => {
@@ -10,4 +11,18 @@ export function defineStore<T>(fn: () => T): () => T {
 
         return sessionData.stores.get(fn) as T
     }
+}
+
+export function defineWritable<T>(
+    value?: unknown,
+    start?: StartStopNotifier<unknown>,
+): () => Writable<unknown> {
+    return defineStore(() => writable(value, start))
+}
+
+export function defineReadable<T>(
+    value?: unknown,
+    start?: StartStopNotifier<unknown>,
+): () => Readable<unknown> {
+    return defineStore(() => readable(value, start))
 }
