6832e295e02eb6bbfc1ede30e73713e1745e3999
^x3ro <git@x3ro.dev>
2022-01-06 12:44:26 +0100

    🐛 FIX: Create session data when using store without call to `useLoad`

--- src/useSession.ts
+++ src/useSession.ts
@@ -1,5 +1,4 @@
 import { session as sessionStore } from '$app/stores'
-//import { session as sessionStore } from '@sveltejs/kit/assets/runtime/app/stores'
 import { get as $ } from 'svelte/store'
 import type { Session } from './svelteKitTypes'
 import { loadSession, sessionMap } from './useLoad'
@@ -8,11 +7,15 @@ import type { SessionData } from './useLoad'
 export function useSession(): { session: Session; sessionData: SessionData } {
     const session = loadSession ?? ($(sessionStore) as Session)
 
-    const sessionData = sessionMap.get(session)
-
-    if (!sessionData) {
-        throw new Error('Call useLoad before calls to useSession')
+    if (!session) {
+        throw new Error('Failed to get session')
     }
+
+    if (!sessionMap.has(session)) {
+        // TODO: `fetch: fetch` instead of `null`?
+        sessionMap.set(session, { stores: new Map(), fetch: null })
+    }
+    const sessionData = sessionMap.get(session)
 
     return {
         session,
