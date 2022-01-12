213687873f9b750d663c18e67400498988116335
^x3ro <git@x3ro.dev>
2022-01-12 13:14:29 +0100

    🐛 FIX: Add missing `const` to error message

--- src/loadWithStoresHint.ts
+++ src/loadWithStoresHint.ts
@@ -1,2 +1,2 @@
 export const LOAD_WITH_STORES_HINT =
-    'Please do `export load = loadWithStores(...)` in the `module` section of the current layout/page first.'
+    'Please do `export const load = loadWithStores(...)` in the `module` section of the current layout/page first.'
